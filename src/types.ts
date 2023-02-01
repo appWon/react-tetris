910ea99f07339735825f2696a97ceaf0d746b9b4
dev-Jaewon <app235naver@gmail.com>
2023-02-01 11:30:28 +0900

    feat 타입 파일 init

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+import { LEFT_OR_RIGHT } from "./constants";
+
+export type BlockType = {
+    color: string;
+    state: "blank" | "fixed" | "drop" | "duplicated" | "end";
+};
+
+export type RowWidth = { x: number; y: number };
+
+export type LeftOrRight = typeof LEFT_OR_RIGHT[keyof typeof LEFT_OR_RIGHT];
