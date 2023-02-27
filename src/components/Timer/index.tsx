dd34b5aa8cad2468a09588894a2a3484b2f79d1e
dev-Jaewon <app235naver@gmail.com>
2023-02-27 10:27:07 +0900

    react - 불필요 코드 제거 및 간소화

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,48 +1,27 @@
-import React, {
-    Dispatch,
-    ReactNode,
-    SetStateAction,
-    useEffect,
-    useState,
-} from "react";
+import { useEffect, useRef, useState } from "react";
 
 type TimerProps = {
-    trigger: boolean;
-    children: ReactNode;
-    onSetTrigger: Dispatch<SetStateAction<boolean>>;
     executeFn: () => void;
 };
 
 export const Timer = (props: TimerProps) => {
-    const [text, setText] = useState<string | number>("");
+    const [time, setTime] = useState<number>(3);
+    const intervalRef = useRef<number | undefined>(undefined);
 
     useEffect(() => {
-        if (!props.trigger) return;
-
-        setText(3);
-    }, [props.trigger]);
-
-    useEffect(() => {
-        if (!text) return;
-
-        const timer = setTimeout(() => {
-            if (text === 1) setText("go");
-            else if (typeof text === "number") setText(text - 1);
-            else {
-                setText("");
-                props.executeFn();
-                props.onSetTrigger(false);
-            }
+        intervalRef.current = setInterval(() => {
+            setTime((prevTime) => prevTime - 1);
         }, 1000);
+    }, []);
 
-        return () => {
-            clearTimeout(timer);
-        };
-    }, [text]);
+    useEffect(() => {
+        if (time === -1 && intervalRef.current) {
+            props.executeFn();
+            clearInterval(intervalRef.current);
+        }
 
-    return (
-        <>
-            <p> {text || props.children}</p>
-        </>
-    );
+        () => clearInterval(intervalRef.current);
+    }, [time]);
+
+    return <p>{time > 0 ? time : "Start!"}</p>;
 };
