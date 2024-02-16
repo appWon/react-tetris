9d1ea52052af863bd002822dcc5a3e6f9a531c2e
dev-Jaewon <app235naver@gmail.com>
2024-02-17 06:21:37 +0900

    move level 표시 component 이동

--- app/components/atoms/Level.tsx
+++ app/components/atoms/Level.tsx
@@ -9,7 +9,9 @@ export const Level = () => {
     <section className="flex flex-col justify-center items-center font-main text-[30px] w-[130px] text-white p-[10px] rounded-[10px] bg-m-black select-none gap-[5px]">
       <h3 className="text-[red]">LEVEL</h3>
       <h4>{timeFrame}</h4>
-      <p className="text-[15px] p-t-[5px] p-b-[0] text-center">20초마다 레벨 상승</p>
+      <p className="text-[15px] p-t-[5px] p-b-[0] text-center">
+        20초마다 레벨 상승
+      </p>
     </section>
   );
 };
