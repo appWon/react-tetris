6773b93d18b38f071ecf69efc3ba9075dd711521
dev-Jaewon <app235naver@gmail.com>
2024-02-17 07:06:16 +0900

    remove App component 제거

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
-import App from './App'
+"use client";
+import { Tetris } from "./components/templates/Game";
+import { NickName } from "./components/molecules/NickName";
+
+// redux
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <App />
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Provider store={store}>
+        <div className="flex justify-center items-center w-screen min-h-screen gap-[20px] bg-[gray]">
+          <NickName />
+          <Tetris />
+        </div>
+      </Provider>
     </main>
   );
 }
