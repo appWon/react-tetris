6ef1b1bc1fa62ec69f8269e5f6b95aebafa1c26b
dev-Jaewon <app235naver@gmail.com>
2024-02-17 06:16:26 +0900

    fix comfile 길어진 문제 해결

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,7 @@ import plugin from "tailwindcss/plugin";
 
 const config: Config = {
   content: [
-    "**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
