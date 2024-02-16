d3d71869b600e4682b1e45e3e2416d12a8235464
dev-Jaewon <app235naver@gmail.com>
2024-02-17 06:36:09 +0900

    move timer component 이동

---
+++ app/components/atoms/Timer.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useRef, useState } from "react";
+
+type TimerProps = {
+  executeFn: () => void;
+};
+
+export const Timer = (props: TimerProps) => {
+  const [time, setTime] = useState<number>(3);
+  const intervalRef = useRef<number | undefined>(undefined);
+
+  useEffect(() => {
+    intervalRef.current = window.setInterval(() => {
+      setTime((prevTime) => prevTime - 1);
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
+    if (time === -1 && intervalRef.current) {
+      props.executeFn();
+      window.clearInterval(intervalRef.current);
+    }
+
+    () => window.clearInterval(intervalRef.current);
+  }, [time]);
+
+  return <p>{time > 0 ? time : "Start!"}</p>;
+};
